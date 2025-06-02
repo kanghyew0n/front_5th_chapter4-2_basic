37ad2fcea553f7bae086054bc56b8ffe30abc88f
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-06-03 01:31:20 +0900

    chore: fetch 개선

--- js/products.js
+++ js/products.js
@@ -1,7 +1,11 @@
 async function loadProducts() {
-  const response = await fetch("https://fakestoreapi.com/products");
-  const products = await response.json();
-  displayProducts(products);
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const products = await response.json();
+    displayProducts(products);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function displayProducts(products) {
